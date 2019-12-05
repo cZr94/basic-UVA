e0f5190f0698f59d2d2c65438df0cd4aa89457fe
cZr94 <57312182+cZr94@users.noreply.github.com>
2019-12-05 14:19:37 +0600

    Compiler error

---
+++ 865.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int tcase, loop;
+	char A[100], B[100];
+	scanf("%d", &tcase);
+	getchar();
+	getchar();
+	while(tcase--)
+    {
+		gets(A);
+		gets(B);
+		char ASCII[128];
+
+		int lenA = strlen(A);
+        int lenB = strlen(B);
+
+		for(loop = 0; loop < 128; loop++)
+			ASCII[loop] = loop;
+		for(loop = 0; loop < lenA || loop < lenB; loop++)
+        {
+			if(loop >= lenA)
+                A[loop] = ' ';
+			if(loop >= lenB)
+                B[loop] = ' ';
+
+			ASCII[A[loop]] = B[loop];
+		}
+
+		puts(B);
+		puts(A);
+
+		while(gets(A))
+		{
+			if(A[0] == '\0')
+				break;
+
+			for(loop = 0; A[loop]; loop++)
+			{
+				printf("%c", ASCII[A[loop]]);
+			}
+			puts("");
+		}
+		if(tcase)
+			puts("");
+	}
+    return 0;
+}
